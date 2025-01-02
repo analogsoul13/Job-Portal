d48d4195e06a4806346e221c3d639fadb1f28619
analogsoul13 <analogsoul13@gmail.com>
2025-01-02 21:14:02 +0530

    Layout changes

--- src/components/Candidate/AppliedJobList.jsx
+++ src/components/Candidate/AppliedJobList.jsx
@@ -6,7 +6,7 @@ function AppliedJobList() {
     <>
       <div className='max-w-full bg-base-100 flex flex-col h-screen p-4 rounded-lg shadow-xl'>
         <h1 className='text-xl bg-base-200 rounded-lg p-2 shadow font-semibold text-center'>Jobs Applied</h1>
-        <div className='flex my-4 space-x-2 py-4 shadow bg-base-200 rounded-lg'>
+        <div className='grid sm:grid-cols-3 my-4 px-2 space-x-2 py-4 shadow bg-base-200 rounded-lg'>
           <JobCard/>
           <JobCard/>
           <JobCard/>
--- src/components/Candidate/FindJobs.jsx
+++ src/components/Candidate/FindJobs.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import JobCard from '../JobCard'
+import JobFilter from '../JobFilter'
+import SearchBar from '../SearchBar'
 
 function FindJobs() {
   return (
     <>
-    <div className='max-w-full grid grid-cols-3 gap-6 bg-base-100 p-4 rounded-lg shadow-xl'>
-        <div className='col-span-1'><JobCard/></div>
-        <div><JobCard/></div>
-        <div><JobCard/></div>
-        <div><JobCard/></div>
-        <div><JobCard/></div>
-        <div><JobCard/></div>
-    </div>
+      <div className='flex flex-col space-y-2'>
+        <div><SearchBar /></div>
+        <div className='max-w-full grid sm:grid-cols-3 gap-6 bg-base-100 p-4 rounded-lg shadow-xl'>
+          <div className='col-span-1'><JobCard /></div>
+          <div><JobCard /></div>
+          <div><JobCard /></div>
+          <div><JobCard /></div>
+          <div><JobCard /></div>
+          <div><JobCard /></div>
+        </div>
+      </div>
+
     </>
   )
 }
