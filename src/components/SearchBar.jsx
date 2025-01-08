7cca001d4fa2938fc25827566a84650980165b2b
analogsoul13 <analogsoul13@gmail.com>
2025-01-08 22:41:50 +0530

    Hidden on large screen

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,7 +14,7 @@ function SearchBar() {
             <div className="bg-base-100 shadow-xl p-2 rounded-lg">
                 <input type="text" className="w-full input input-bordered p-3 rounded-lg" placeholder="Search for jobs..." />
                 {/* <h2 className="mt-4 text-2xl font-semibold">Welcome, <span className='text-accent'>Anne Hathaway</span></h2> */}
-                <button onClick={toggleFilter} className='btn btn-xs text-base-content btn-link'>Filter</button>
+                <button onClick={toggleFilter} className='btn md:hidden btn-xs text-base-content btn-link'>Filter</button>
 
                 {/* Filter Card Rendering */}
                 {
