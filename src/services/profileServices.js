dd7fb59e994b284f155d47e09381afd67c762035
analogsoul13 <analogsoul13@gmail.com>
2025-06-17 19:10:22 +0530

    Optimized

--- src/services/profileServices.js
+++ src/services/profileServices.js
@@ -1,10 +1,11 @@
 import BASE_URL from "./baseUrl";
 import commonApi from "./commonApi";
 
-export const getUserProfile = async () => {
-    try {
-        return await commonApi(`${BASE_URL}/api/v1/user/profile`, "GET");  // No token needed
-    } catch (error) {
-        throw error.response?.data || { message: 'Something went wrong' };
-    }
+export const getUserProfile = async (headers) => {
+    return await commonApi(`${BASE_URL}/api/v1/user/profile`, "GET", headers);
+
+};
+
+export const updateProfileApi = async (profileData, headers ) => {
+    return await commonApi(`${BASE_URL}/api/v1/user/profile/update`, "PUT", headers, profileData);
 };
