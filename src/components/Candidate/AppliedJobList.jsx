a19dddfbdb496551c4124df9f1fd4337519817dd
analogsoul13 <analogsoul13@gmail.com>
2025-06-20 23:25:00 +0530

    Fetched applied jobs

--- src/components/Candidate/AppliedJobList.jsx
+++ src/components/Candidate/AppliedJobList.jsx
@@ -1,38 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import JobCard from '../JobCard'
+import { useSelector } from 'react-redux'
+import { getAppliedJobsApi } from '../../services/jobServices'
 
 function AppliedJobList() {
+  const [appliedJobs, setAppliedJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+  const token = useSelector((state) => state.auth.token)
+
+  useEffect(() => {
+    const fetchAppliedJobs = async () => {
+      try {
+        const headers = {
+          Authorization: `Bearer ${token}`
+        }
+        const res = await getAppliedJobsApi(headers)
+        console.log("Fetched Applications : ", res);
+
+        setAppliedJobs(res.data.application)
+        setLoading(false)
+      } catch (error) {
+        console.error('Failed to fetch applied jobs', err)
+      } finally {
+        setLoading(false)
+      }
+    }
+    if (token) fetchAppliedJobs()
+  }, [token])
+
+  if (loading) return <div className="flex justify-center h-full items-center"><span className="loading loading-bars loading-xl"></span></div>;
   return (
     <>
       <div className='max-w-full bg-base-100 flex flex-col h-screen p-4 rounded-lg shadow-xl'>
         <h1 className='text-xl bg-base-200 rounded-lg p-2 shadow font-semibold text-center'>Jobs Applied</h1>
         <div className='grid sm:grid-cols-3 my-4 space-x-2 px-3 py-4 bg-base-200 rounded-lg'>
+          {appliedJobs?.length > 0 ? (
+            appliedJobs.map((app) =>
+              <JobCard
+                key={app._id}
+                job={app.job}
+                showApplyButton={false}
+                applicationStatus={app.status} />)
+          ) : (
+            <p className='text-center col-span-3'>No jobs found.</p>
+          )}
 
-        <div className='card rounded-lg border bg-base-100 fade-in hover:shadow-lg overflow-hidden w-auto h-full'>
-                <div className="card-body">
-                    <div className='flex justify-between'>
-                        <img className='rounded-full h-6 w-6' src="https://e7.pngegg.com/pngimages/882/225/png-clipart-google-logo-google-logo-google-search-icon-google-text-logo.png" alt="" />
-                        <div className="badge badge-success text-slate-100 text-xs">Applied</div>
-                    </div>
-
-                    <h4>Google LLM</h4>
-                    <h2 className="card-title text-slate-700">Software Developer</h2>
-                    <div className='flex justify-between'>
-                        <p className='text-xs text-slate-500'><i className="fa-solid fa-location-dot me-2" />Mumbai</p>
-                        <p className='text-xs text-slate-500'><i className="fa-solid fa-clock me-2" />Full-Time</p>
-                        <p className='text-xs text-slate-500'><i className="fa-solid fa-sack-dollar me-2" />45LPA</p>
-                    </div>
-                    <div>
-                        <h3 className='text-sm text-slate-600'>Job Description</h3>
-                        <p className='text-xs text-slate-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, incidunt!</p>
-                    </div>
-                    <div>
-                        <button className='btn hover:shadow-xl btn-accent text-slate-50'>Apply Now</button>
-                    </div>
-
-
-                </div>
-            </div>
 
         </div>
       </div>
