7eb496123038bb89cd9b8192083b5d843a1af550
analogsoul13 <analogsoul13@gmail.com>
2025-07-19 21:04:56 +0530

    PostJob as Component

--- src/components/Dashboard/HeroSectioRecruiter.jsx
+++ src/components/Dashboard/HeroSectioRecruiter.jsx
@@ -1,118 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import PostJob from '../Recruiter/PostJob'
 
 function HeroSectioRecruiter() {
-    const [isModalOpen, setIsModalOpen] = useState(false)
     return (
         <>
             <div className='flex mt-4 flex-col bg-base-100 rounded-lg shadow-xl justify-center mx-4 space-y-3 items-center p-4'>
                 <h1 className='text-base-content text-center text-xl'>Dashboard</h1>
                 <div className='grid w-full sm:grid-cols-2 gap-4'>
                     {/* Modal */}
-                    <div className='flex justify-center items-center bg-base-200 rounded-lg p-4 col-span-1'>
-                        <button onClick={() => { setIsModalOpen(true) }} className='btn btn-neutral'><i className="fa-solid fa-square-plus" />Post an Opening</button>
-                        {isModalOpen && (
-                            <div className="fixed inset-0 bg-gray-900 bg-opacity-50 fade-in flex items-center justify-center z-50">
-                                <div className="bg-base-100 w-full max-w-lg rounded-lg shadow-lg p-6">
-                                    <h2 className="text-2xl font-semibold mb-4">Create Job Opening</h2>
-
-                                    <form>
-                                        {/* Job Title */}
-                                        <div className="mb-4">
-                                            <label className="block text-sm font-medium">Job Title</label>
-                                            <input
-                                                type="text"
-                                                required
-                                                placeholder="Enter job title"
-                                                className="mt-1 w-full border rounded-md p-2"
-                                            />
-                                        </div>
-
-                                        {/* Description */}
-                                        <div className="mb-4">
-                                            <label className="block text-sm font-medium">Description</label>
-                                            <textarea
-                                                required
-                                                placeholder="Enter job description"
-                                                className="mt-1 w-full border rounded-md p-2"
-                                            ></textarea>
-                                        </div>
-
-                                        <div className='flex justify-between gap-2'>
-                                            {/* Location */}
-                                            <div className="mb-4">
-                                                <label className="block text-sm font-medium">Location</label>
-                                                <input
-                                                    type="text"
-                                                    placeholder="Enter job location"
-                                                    className="mt-1 w-full border rounded-md p-2"
-                                                />
-                                            </div>
-                                            {/* Salary */}
-                                            <div className="mb-4">
-                                                <label className="block text-sm font-medium">Salary</label>
-                                                <input
-                                                    type="number"
-                                                    placeholder="Enter Salary"
-                                                    className="mt-1 w-full border rounded-md p-2"
-                                                />
-                                            </div>
-                                        </div>
-
-                                        <div className='flex items-center justify-between'>
-                                            {/* Job Type */}
-                                            <div className="mb-4">
-                                                <label className="block text-sm font-medium">Job Type</label>
-                                                <select className="mt-1 w-full border rounded-md p-2">
-                                                    <option>Full-time</option>
-                                                    <option>Part-time</option>
-                                                    <option>Remote</option>
-                                                    <option>Hybrid</option>
-                                                </select>
-                                            </div>
-
-                                            {/* Experience Level */}
-                                            <div className="mb-4">
-                                                <label className="block text-sm font-medium">Experience Level</label>
-                                                <select className="mt-1 w-full border rounded-md p-2">
-                                                    <option>Any</option>
-                                                    <option>Minimum 1year</option>
-                                                    <option>2+ Years</option>
-                                                    <option>5+ Years</option>
-                                                </select>
-                                            </div>
-
-                                            {/* Openings */}
-                                            <div className="mb-4">
-                                                <label className="block text-sm font-medium">No of Openings</label>
-                                                <input
-                                                    type="number"
-                                                    placeholder="Enter Openings"
-                                                    className="mt-1 border rounded-md p-2"
-                                                />
-                                            </div>
-                                        </div>
-
-
-                                        {/* Buttons */}
-                                        <div className="flex justify-end space-x-4">
-                                            <button
-                                                type="button"
-                                                onClick={() => setIsModalOpen(false)}
-                                                className="btn btn-error">
-                                                Cancel
-                                            </button>
-                                            <button
-                                                type="submit"
-                                                className="btn btn-accent">
-                                                Create
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        )}
-
-                    </div>
+                    <PostJob/>
                     {/* ViewJObs */}
                     <div className='flex justify-center bg-base-200 rounded-lg p-4'>
                         <button className='btn btn-accent'><i className="fa-solid fa-list" />View All Jobs Posted</button>
